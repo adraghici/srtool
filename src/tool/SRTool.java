2a9a54e9dc91590880b74592b2abba7e505d9d6c
Constantin Mateescu <costica1234@me.com>
2015-10-30 03:27:53 +0000

    Basic implementation for the variable shadowing visitor - WIP.

--- src/tool/SRTool.java
+++ src/tool/SRTool.java
@@ -14,73 +14,76 @@ public class SRTool {
 
     private static final int TIMEOUT = 30;
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         // First pass through initial SimpleC file.
-		String filename = args[0];
-		ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(filename));
-		ProgramContext ctx = getProgramContext(input, filename);
+        String filename = args[0];
+        ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(filename));
+        ProgramContext ctx = getProgramContext(input, filename);
 
-		// Second pass through the file after renaming shadow variables.
-		SRToolShadowing srToolShadowing = new SRToolShadowing();
-		input = new ANTLRInputStream(srToolShadowing.visit(ctx));
-		// ctx = getProgramContext(input, filename);
-		
-		assert ctx.procedures.size() == 1; // For Part 1 of the coursework, this can be assumed
+        // Second pass through the file after renaming shadow variables.
+        SRToolShadowing srToolShadowing = new SRToolShadowing();
+        String content = srToolShadowing.visit(ctx);
+        System.out.println(content);
 
-		VCGenerator vcgen = new VCGenerator(ctx);
-		String vc = vcgen.generateVC().toString();
+        input = new ANTLRInputStream(content);
+        ctx = getProgramContext(input, filename);
 
-		String dir = System.getProperty("user.dir");
-		String tool = "srtool";
-		dir = dir.substring(0, dir.lastIndexOf(tool) + tool.length());
-		ProcessExec process = new ProcessExec(dir + "/z3", "-smt2", "-in");
-		String queryResult = "";
-		try {
-			queryResult = process.execute(vc, TIMEOUT);
-			// System.out.println(queryResult);
-		} catch (ProcessTimeoutException e) {
-			System.out.println("UNKNOWN");
-			System.exit(1);
-		}
+        assert ctx.procedures.size() == 1; // For Part 1 of the coursework, this can be assumed
 
-		if (queryResult.startsWith("sat")) {
-			System.out.println("INCORRECT");
-			System.exit(0);
-		}
+        VCGenerator vcgen = new VCGenerator(ctx);
+        String vc = vcgen.generateVC().toString();
 
-		if (!queryResult.startsWith("unsat")) {
-			System.out.println("UNKNOWN");
-			System.out.println(queryResult);
-			System.exit(1);
-		}
-		
-		System.out.println("CORRECT");
-		System.exit(0);
-		
+        String dir = System.getProperty("user.dir");
+        String tool = "srtool";
+        dir = dir.substring(0, dir.lastIndexOf(tool) + tool.length());
+        ProcessExec process = new ProcessExec(dir + "/z3", "-smt2", "-in");
+        String queryResult = "";
+        try {
+            queryResult = process.execute(vc, TIMEOUT);
+            // System.out.println(queryResult);
+        } catch (ProcessTimeoutException e) {
+            System.out.println("UNKNOWN");
+            System.exit(1);
+        }
+
+        if (queryResult.startsWith("sat")) {
+            System.out.println("INCORRECT");
+            System.exit(0);
+        }
+
+        if (!queryResult.startsWith("unsat")) {
+            System.out.println("UNKNOWN");
+            System.out.println(queryResult);
+            System.exit(1);
+        }
+
+        System.out.println("CORRECT");
+        System.exit(0);
+
     }
 
-	private static ProgramContext getProgramContext(ANTLRInputStream input, String filename) {
-		SimpleCLexer lexer = new SimpleCLexer(input);
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		SimpleCParser parser = new SimpleCParser(tokens);
-		ProgramContext ctx = parser.program();
+    private static ProgramContext getProgramContext(ANTLRInputStream input, String filename) {
+        SimpleCLexer lexer = new SimpleCLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        SimpleCParser parser = new SimpleCParser(tokens);
+        ProgramContext ctx = parser.program();
 
-		if(parser.getNumberOfSyntaxErrors() > 0) {
-			System.exit(1);
-		}
+        if(parser.getNumberOfSyntaxErrors() > 0) {
+            System.exit(1);
+        }
 
-		Typechecker tc = new Typechecker();
-		tc.visit(ctx);
-		tc.resolve();
+        Typechecker tc = new Typechecker();
+        tc.visit(ctx);
+        tc.resolve();
 
-		if(tc.hasErrors()) {
-			System.err.println("Errors were detected when typechecking " + filename + ":");
-			for(String err : tc.getErrors()) {
-				System.err.println("  " + err);
-			}
-			System.exit(1);
-		}
+        if(tc.hasErrors()) {
+            System.err.println("Errors were detected when typechecking " + filename + ":");
+            for(String err : tc.getErrors()) {
+                System.err.println("  " + err);
+            }
+            System.exit(1);
+        }
 
-		return ctx;
-	}
+        return ctx;
+    }
 }
--- src/tool/SRToolShadowing.java
+++ src/tool/SRToolShadowing.java
@@ -1,10 +1,53 @@
 package tool;
 
 import parser.SimpleCBaseVisitor;
-import parser.SimpleCParser.*;
+import parser.SimpleCParser.AddExprContext;
+import parser.SimpleCParser.AssertStmtContext;
+import parser.SimpleCParser.AssignStmtContext;
+import parser.SimpleCParser.AssumeStmtContext;
+import parser.SimpleCParser.AtomExprContext;
+import parser.SimpleCParser.BandExprContext;
+import parser.SimpleCParser.BlockStmtContext;
+import parser.SimpleCParser.BorExprContext;
+import parser.SimpleCParser.BxorExprContext;
+import parser.SimpleCParser.CallStmtContext;
+import parser.SimpleCParser.CandidateEnsuresContext;
+import parser.SimpleCParser.CandidateInvariantContext;
+import parser.SimpleCParser.CandidateRequiresContext;
+import parser.SimpleCParser.EnsuresContext;
+import parser.SimpleCParser.EqualityExprContext;
+import parser.SimpleCParser.ExprContext;
+import parser.SimpleCParser.FormalParamContext;
+import parser.SimpleCParser.HavocStmtContext;
+import parser.SimpleCParser.IfStmtContext;
+import parser.SimpleCParser.InvariantContext;
+import parser.SimpleCParser.LandExprContext;
+import parser.SimpleCParser.LoopInvariantContext;
+import parser.SimpleCParser.LorExprContext;
+import parser.SimpleCParser.MulExprContext;
+import parser.SimpleCParser.NumberExprContext;
+import parser.SimpleCParser.OldExprContext;
+import parser.SimpleCParser.ParenExprContext;
+import parser.SimpleCParser.PrepostContext;
+import parser.SimpleCParser.ProcedureDeclContext;
+import parser.SimpleCParser.ProgramContext;
+import parser.SimpleCParser.RelExprContext;
+import parser.SimpleCParser.RequiresContext;
+import parser.SimpleCParser.ResultExprContext;
+import parser.SimpleCParser.ShiftExprContext;
+import parser.SimpleCParser.StmtContext;
+import parser.SimpleCParser.TernExprContext;
+import parser.SimpleCParser.UnaryExprContext;
+import parser.SimpleCParser.VarDeclContext;
+import parser.SimpleCParser.VarIdentifierContext;
+import parser.SimpleCParser.VarrefContext;
+import parser.SimpleCParser.VarrefExprContext;
+import parser.SimpleCParser.WhileStmtContext;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SRToolShadowing extends SimpleCBaseVisitor<String> {
-
 
     public SRToolShadowing() {
 
@@ -12,37 +55,75 @@ public class SRToolShadowing extends SimpleCBaseVisitor<String> {
 
     @Override
     public String visitProgram(ProgramContext ctx) {
-        return "";
+        List<String> globals = ctx.globals.stream().map(this::visit).collect(Collectors.toList());
+        List<String> procedures = ctx.procedures.stream().map(this::visit).collect(Collectors.toList());
+        return String.join("", globals) + String.join("", procedures);
     }
 
     @Override
     public String visitVarDecl(VarDeclContext ctx) {
-        return "";
+        return "int " + ctx.ident.name.getText() + ";";
     }
 
     @Override
     public String visitProcedureDecl(ProcedureDeclContext ctx) {
-        return "";
+        StringBuilder result = new StringBuilder();
+        result.append("int " + ctx.name.getText() + "(");
+
+        for (FormalParamContext formal : ctx.formals) {
+            result.append(visit(formal) + ", ");
+        }
+
+        if (ctx.formals.size() != 0) {
+            result.delete(result.length() - 2, result.length());
+        }
+        result.append(")\n");
+
+        for (PrepostContext prepost : ctx.contract) {
+            result.append("    " + visit(prepost) + ",\n");
+        }
+
+        if (ctx.contract.size() != 0) {
+            result.delete(result.length() - 2, result.length());
+            result.append("\n");
+        }
+        result.append("{\n");
+
+        for (StmtContext stmt : ctx.stmts) {
+            result.append(visit(stmt) + "\n");
+        }
+        result.append("return " + visit(ctx.returnExpr) + ";\n}");
+
+        return result.toString();
     }
 
     @Override
     public String visitFormalParam(FormalParamContext ctx) {
-        return "";
+        return "int " + visit(ctx.ident);
     }
 
     @Override
     public String visitPrepost(PrepostContext ctx) {
-        return "";
+        StringBuilder result = new StringBuilder();
+        if (ctx.requires() != null) {
+            result.append(visit(ctx.requires()));
+        }
+
+        if (ctx.ensures() != null) {
+            result.append(visit(ctx.ensures()));
+        }
+
+        return result.toString();
     }
 
     @Override
     public String visitRequires(RequiresContext ctx) {
-        return "";
+        return "requires " + visit(ctx.condition);
     }
 
     @Override
     public String visitEnsures(EnsuresContext ctx) {
-        return "";
+        return "ensures " + visit(ctx.condition);
     }
 
     @Override
@@ -57,27 +138,59 @@ public class SRToolShadowing extends SimpleCBaseVisitor<String> {
 
     @Override
     public String visitStmt(StmtContext ctx) {
-        return "";
+        if (ctx.varDecl() != null) {
+            return visit(ctx.varDecl());
+        }
+
+        if (ctx.assignStmt() != null) {
+            return visit(ctx.assignStmt());
+        }
+
+        if (ctx.assertStmt() != null) {
+            return visit(ctx.assertStmt());
+        }
+
+        if (ctx.assumeStmt() != null) {
+            return visit(ctx.assumeStmt());
+        }
+
+        if (ctx.havocStmt() != null) {
+            return visit(ctx.havocStmt());
+        }
+
+        if (ctx.callStmt() != null) {
+            return visit(ctx.callStmt());
+        }
+
+        if (ctx.ifStmt() != null) {
+            return visit(ctx.ifStmt());
+        }
+
+        if (ctx.whileStmt() != null) {
+            return visit(ctx.whileStmt());
+        }
+
+        return visit(ctx.blockStmt());
     }
 
     @Override
     public String visitAssignStmt(AssignStmtContext ctx) {
-        return "";
+        return ctx.lhs.ident.name.getText() + " = " + visit(ctx.rhs) + ";";
     }
 
     @Override
     public String visitAssertStmt(AssertStmtContext ctx) {
-        return "";
+        return "assert " + visit(ctx.condition) + ";";
     }
 
     @Override
     public String visitAssumeStmt(AssumeStmtContext ctx) {
-        return "";
+        return "assume " + visit(ctx.condition) + ";";
     }
 
     @Override
     public String visitHavocStmt(HavocStmtContext ctx) {
-        return "";
+        return "havoc " + visit(ctx.var) + ";";
     }
 
     @Override
@@ -87,7 +200,14 @@ public class SRToolShadowing extends SimpleCBaseVisitor<String> {
 
     @Override
     public String visitIfStmt(IfStmtContext ctx) {
-        return "";
+        StringBuilder result = new StringBuilder();
+        result.append("if (" + visit(ctx.condition) + ") " + visit(ctx.thenBlock));
+        if (ctx.elseBlock != null) {
+            result.append(" else ");
+            result.append(visit(ctx.elseBlock));
+        }
+
+        return result.toString();
     }
 
     @Override
@@ -97,7 +217,8 @@ public class SRToolShadowing extends SimpleCBaseVisitor<String> {
 
     @Override
     public String visitBlockStmt(BlockStmtContext ctx) {
-        return "";
+        List<String> statements = ctx.stmt().stream().map(this::visit).collect(Collectors.toList());
+        return "{\n" + String.join("\n", statements) + "\n}";
     }
 
     @Override
@@ -117,107 +238,211 @@ public class SRToolShadowing extends SimpleCBaseVisitor<String> {
 
     @Override
     public String visitExpr(ExprContext ctx) {
-        return "";
+        return visit(ctx.ternExpr());
     }
 
     @Override
     public String visitTernExpr(TernExprContext ctx) {
+        if (ctx.single != null) {
+            return visit(ctx.single);
+        }
+
         return "";
     }
 
     @Override
     public String visitLorExpr(LorExprContext ctx) {
-        return "";
+        if (ctx.single != null) {
+            return visit(ctx.single);
+        }
+
+        List<String> args = ctx.args.stream().map(this::visit).collect(Collectors.toList());
+        List<String> ops = ctx.ops.stream().map(t -> t.getText()).collect(Collectors.toList());
+        return binaryExpr(args, ops);
     }
 
     @Override
     public String visitLandExpr(LandExprContext ctx) {
-        return "";
+        if (ctx.single != null) {
+            return visit(ctx.single);
+        }
+
+        List<String> args = ctx.args.stream().map(this::visit).collect(Collectors.toList());
+        List<String> ops = ctx.ops.stream().map(t -> t.getText()).collect(Collectors.toList());
+        return binaryExpr(args, ops);
     }
 
     @Override
     public String visitBorExpr(BorExprContext ctx) {
-        return "";
+        if (ctx.single != null) {
+            return visit(ctx.single);
+        }
+
+        List<String> args = ctx.args.stream().map(this::visit).collect(Collectors.toList());
+        List<String> ops = ctx.ops.stream().map(t -> t.getText()).collect(Collectors.toList());
+        return binaryExpr(args, ops);
     }
 
     @Override
     public String visitBxorExpr(BxorExprContext ctx) {
-        return "";
+        if (ctx.single != null) {
+            return visit(ctx.single);
+        }
+
+        List<String> args = ctx.args.stream().map(this::visit).collect(Collectors.toList());
+        List<String> ops = ctx.ops.stream().map(t -> t.getText()).collect(Collectors.toList());
+        return binaryExpr(args, ops);
     }
 
     @Override
     public String visitBandExpr(BandExprContext ctx) {
-        return "";
+        if (ctx.single != null) {
+            return visit(ctx.single);
+        }
+
+        List<String> args = ctx.args.stream().map(this::visit).collect(Collectors.toList());
+        List<String> ops = ctx.ops.stream().map(t -> t.getText()).collect(Collectors.toList());
+        return binaryExpr(args, ops);
     }
 
     @Override
     public String visitEqualityExpr(EqualityExprContext ctx) {
-        return "";
+        if (ctx.single != null) {
+            return visit(ctx.single);
+        }
+
+        List<String> args = ctx.args.stream().map(this::visit).collect(Collectors.toList());
+        List<String> ops = ctx.ops.stream().map(t -> t.getText()).collect(Collectors.toList());
+        return binaryExpr(args, ops);
     }
 
     @Override
     public String visitRelExpr(RelExprContext ctx) {
-        return "";
+        if (ctx.single != null) {
+            return visit(ctx.single);
+        }
+
+        List<String> args = ctx.args.stream().map(this::visit).collect(Collectors.toList());
+        List<String> ops = ctx.ops.stream().map(t -> t.getText()).collect(Collectors.toList());
+        return binaryExpr(args, ops);
     }
 
     @Override
     public String visitShiftExpr(ShiftExprContext ctx) {
-        return "";
+        if (ctx.single != null) {
+            return visit(ctx.single);
+        }
+
+        List<String> args = ctx.args.stream().map(this::visit).collect(Collectors.toList());
+        List<String> ops = ctx.ops.stream().map(t -> t.getText()).collect(Collectors.toList());
+        return binaryExpr(args, ops);
     }
 
     @Override
     public String visitAddExpr(AddExprContext ctx) {
-        return "";
+        if (ctx.single != null) {
+            return visit(ctx.single);
+        }
+
+        List<String> args = ctx.args.stream().map(this::visit).collect(Collectors.toList());
+        List<String> ops = ctx.ops.stream().map(t -> t.getText()).collect(Collectors.toList());
+        return binaryExpr(args, ops);
     }
 
     @Override
     public String visitMulExpr(MulExprContext ctx) {
-        return "";
+        if (ctx.single != null) {
+            return visit(ctx.single);
+        }
+
+        List<String> args = ctx.args.stream().map(this::visit).collect(Collectors.toList());
+        List<String> ops = ctx.ops.stream().map(t -> t.getText()).collect(Collectors.toList());
+        return binaryExpr(args, ops);
     }
 
     @Override
     public String visitUnaryExpr(UnaryExprContext ctx) {
-        return "";
+        if (ctx.single != null) {
+            return visit(ctx.single);
+        }
+
+        String arg = visit(ctx.arg);
+        List<String> ops = ctx.ops.stream().map(t -> t.getText()).collect(Collectors.toList());
+        return unaryOp(ops, arg);
     }
 
     @Override
     public String visitAtomExpr(AtomExprContext ctx) {
-        return "";
+        if (ctx.numberExpr() != null) {
+            return visit(ctx.numberExpr());
+        }
+
+        if (ctx.varrefExpr() != null) {
+            return visit(ctx.varrefExpr());
+        }
+
+        if (ctx.parenExpr() != null) {
+            return visit(ctx.parenExpr());
+        }
+
+        if (ctx.resultExpr() != null) {
+            return visit(ctx.resultExpr());
+        }
+
+        return visit(ctx.oldExpr());
     }
 
     @Override
     public String visitNumberExpr(NumberExprContext ctx) {
-        return "";
+        return ctx.number.getText();
     }
 
     @Override
     public String visitVarrefExpr(VarrefExprContext ctx) {
-        return "";
+        return visit(ctx.var);
     }
 
     @Override
     public String visitParenExpr(ParenExprContext ctx) {
-        return "";
+        return "(" + visit(ctx.arg) + ")";
     }
 
     @Override
     public String visitResultExpr(ResultExprContext ctx) {
-        return "";
+        return ctx.resultTok.getText() + " ";
     }
 
     @Override
     public String visitOldExpr(OldExprContext ctx) {
-        return "";
+        return ctx.oldTok.getText();
     }
 
     @Override
     public String visitVarref(VarrefContext ctx) {
-        return "";
+        return ctx.ident.getText();
     }
 
     @Override
     public String visitVarIdentifier(VarIdentifierContext ctx) {
-        return "";
+        return ctx.name.getText();
+    }
+
+    private String unaryOp(List<String> ops, String arg) {
+        StringBuilder result = new StringBuilder();
+        for (String op : ops) {
+            result.append(op);
+        }
+        result.append(arg);
+        return result.toString();
+    }
+
+    private static String binaryExpr(List<String> args, List<String> ops) {
+        StringBuilder result = new StringBuilder();
+        result.append(args.get(0));
+        for (int i = 0; i < ops.size(); ++i) {
+            result.append(" " + ops.get(i) + " " + args.get(i+1));
+        }
+        return result.toString();
     }
 
 }
