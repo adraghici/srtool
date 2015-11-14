b1393dfb507956a7e70a193a68706ef21ab36fab
Adrian Draghici <draghici.adrian.b@gmail.com>
2015-11-14 17:20:54 +0000

    Removing unnecessary calls in shadowing.

--- src/visitor/ShadowingVisitor.java
+++ src/visitor/ShadowingVisitor.java
@@ -1,10 +1,11 @@
 package visitor;
 
-import ast.*;
+import ast.BlockStmt;
+import ast.Node;
+import ast.ProcedureDecl;
+import ast.VarDeclStmt;
+import ast.VarRef;
 import ssa.Scopes;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Visitor used to disambiguate variables within nested scopes.
@@ -17,17 +18,6 @@ public class ShadowingVisitor implements Visitor {
     }
 
     @Override
-    public Node visit(Program program) {
-        List<VarDeclStmt> newGlobals = program.getGlobalDecls().stream()
-            .map(global -> (VarDeclStmt) global.accept(this))
-            .collect(Collectors.toList());
-        List<ProcedureDecl> newProcedures = program.getProcedureDecls().stream()
-            .map(procedure -> (ProcedureDecl) procedure.accept(this))
-            .collect(Collectors.toList());
-        return new Program(newGlobals, newProcedures);
-    }
-
-    @Override
     public Node visit(VarDeclStmt varDeclStmt) {
         VarRef varRef = varDeclStmt.getVarRef();
         scopes.declareVar(varRef.getVar());
@@ -37,29 +27,17 @@ public class ShadowingVisitor implements Visitor {
     @Override
     public Node visit(ProcedureDecl procedureDecl) {
         scopes.enterScope();
-        List<VarRef> newParams = procedureDecl.getParams().stream()
-            .map(param -> (VarRef) param.accept(this))
-            .collect(Collectors.toList());
-        List<PrePostCondition> newConditions = procedureDecl.getConditions().stream()
-            .map(cond -> (PrePostCondition) cond.accept(this))
-            .collect(Collectors.toList());
-        List<Stmt> newStmts = procedureDecl.getStmts().stream()
-            .map(stmt -> (Stmt) stmt.accept(this))
-            .collect(Collectors.toList());
-        Expr newReturnExpr = (Expr) procedureDecl.getReturnExpr().accept(this);
+        ProcedureDecl result = (ProcedureDecl) visitChildren(procedureDecl);
         scopes.exitScope();
-        return new ProcedureDecl(
-            procedureDecl.getName(), newParams, newConditions, newStmts, newReturnExpr);
+        return result;
     }
 
     @Override
     public Node visit(BlockStmt blockStmt) {
         scopes.enterScope();
-        List<Stmt> newStmts = blockStmt.getStmts().stream()
-            .map(stmt -> (Stmt) stmt.accept(this))
-            .collect(Collectors.toList());
+        BlockStmt result = (BlockStmt) visitChildren(blockStmt);
         scopes.exitScope();
-        return new BlockStmt(newStmts);
+        return result;
     }
 
     @Override
