c833f428313f91091f86f4694b4ba6c521ec163c
Adrian Draghici <draghici.adrian.b@gmail.com>
2015-11-25 19:59:09 +0000

    Updating the lit config to use a 180s timeout per test and using a 1 step depth increase for BMC.

--- src/util/StrategyUtil.java
+++ src/util/StrategyUtil.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
 
 public class StrategyUtil {
     public static final int BMC_START_DEPTH = 5;
-    public static final int BMC_STEP = 3;
+    public static final int BMC_STEP = 1;
 
     /**
      * Returns the list of asserts failed in the given model.
