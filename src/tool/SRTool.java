22eff570fed4b6cd2554313a3305557d5a3e1462
Constantin Mateescu <costica1234@me.com>
2015-11-28 18:52:38 +0000

    170s with 34 slices

--- src/tool/SRTool.java
+++ src/tool/SRTool.java
@@ -16,8 +16,8 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class SRTool {
-    private static final int OVERALL_TIMEOUT = 165000;
-    private static final int TIMEOUT_SLICES = 33;
+    private static final int OVERALL_TIMEOUT = 170000;
+    private static final int TIMEOUT_SLICES = 34;
     private static final int THREADS = 4;
 
     public static void main(String[] args) throws IOException {
